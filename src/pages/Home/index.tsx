2759fff411f945c1bcd906d89aefbaf20757bdc1
Rodrigo Gondin <rodrigo.gevaerd@compasso.com.br>
2024-05-28 23:05:27 -0300

    creating upper part of the homepage

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,6 @@
 import BigCoffee from "../../assets/Big-coffee.svg";
 import { Coffee, Package, ShoppingCart, Timer } from "@phosphor-icons/react";
-import { BenefitsUpperPart, ImageContainerUpperPart, TitlesUpperPart, UpperHomeContainer } from "./styles";
+import { BenefitsUpperPart, IconContainer1, IconContainer2, IconContainer3, IconContainer4, ImageContainerUpperPart, TitlesUpperPart, UpperHomeContainer } from "./styles";
 
 export function Home() {
   return (
@@ -11,22 +11,26 @@ export function Home() {
           <h2>Com o Coffee Delivery você recebe seu café onde estiver, a qualquer hora</h2>
         </TitlesUpperPart>
         <BenefitsUpperPart>
-          <div>
-            <span><ShoppingCart size={20} color="black" weight="fill" /></span>
-            <p>Compra Simples e segura</p>
-          </div>
-          <div>
-            <span><Package size={20} color="black" weight="fill" /></span>
-            <p>Embalagem mantém o café intacto</p>
-          </div>
-          <div>
-            <span><Timer size={20} color="black" weight="fill" /></span>
-            <p>Entrega rápida e rastreada</p>
-          </div>
-          <div>
-            <span><Coffee size={20} color="black" weight="fill" /></span>
-            <p>O café chega fresquinho até você</p>
-          </div>
+          <section>
+            <div>
+              <IconContainer1><ShoppingCart size={20} color="white" weight="fill" /></IconContainer1>
+              <p>Compra Simples e segura</p>
+            </div>
+            <div>
+              <IconContainer2><Package size={20} color="white" weight="fill" /></IconContainer2>
+              <p>Embalagem mantém o café intacto</p>
+            </div>
+          </section>
+          <section>
+            <div>
+              <IconContainer3><Timer size={20} color="white" weight="fill" /></IconContainer3>
+              <p>Entrega rápida e rastreada</p>
+            </div>
+            <div>
+              <IconContainer4><Coffee size={20} color="white" weight="fill" /></IconContainer4>
+              <p>O café chega fresquinho até você</p>
+            </div>
+          </section>
         </BenefitsUpperPart>
       </div>
       <ImageContainerUpperPart>
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -6,29 +6,71 @@ export const UpperHomeContainer = styled.div`
   justify-content: center;
 
   padding-top: 5.3rem;
+  padding-bottom: 5.3rem;
   gap: 3.5rem;
 `
 
 export const TitlesUpperPart = styled.div`
-  max-width: 470px;
+  max-width: 29.375rem;
   margin-bottom: 66px;
 
   h1 {
     font-family: 'Baloo 2', 'Roboto', sans-serif;
-    font-size: 38px;
-    line-height: 48px;
+    font-size: 2.375rem;
+    line-height: 3rem;
     color: ${(props) => props.theme['base-title']};
     margin-bottom: 18px;
   }
 
   h2 {
-    font-size: 18px;
+    font-size: 1.125rem;
     font-weight: 400;
   }
 `
 
 export const BenefitsUpperPart = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+
+  section {
+    display: flex;
+    gap: 2.5rem;
+
+    div {
+      display: flex;
+      align-items: center;
+      gap: 12px;
+    }
+  }
 `
 
+const IconContainerBase = styled.span`
+  padding: 8px;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+`;
+
+export const IconContainer1 = styled(IconContainerBase)`
+  background-color: ${(props) => props.theme['yellow-dark']};
+`;
+
+export const IconContainer2 = styled(IconContainerBase)`
+  background-color: ${(props) => props.theme['base-text']};
+`;
+
+export const IconContainer3 = styled(IconContainerBase)`
+  background-color: ${(props) => props.theme['yellow']};
+`;
+
+export const IconContainer4 = styled(IconContainerBase)`
+  background-color: ${(props) => props.theme['purple']};
+`;
+
 export const ImageContainerUpperPart = styled.div`
+   img {
+    width: 100%;
+    height: auto;
+  }
 `
