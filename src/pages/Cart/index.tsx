a992eaa0cb701a5f30756edeabbcdc3ec260f2a9
Rodrigo Gondin <rodrigo.gevaerd@compasso.com.br>
2024-08-16 00:18:44 -0300

    almost finishing cart layout

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,6 +1,7 @@
-import { MapPinLine } from "@phosphor-icons/react";
-import { AddressPaymentBox, AddressSection, AreaCityUfRow, CartContainer, CartSummary, NumberComplementRow, PaymentSection, TitlesSections } from "./styles";
+import { Bank, CreditCard, CurrencyDollar, MapPinLine, Money, Trash } from "@phosphor-icons/react";
+import { AddressPaymentBox, AddressSection, AreaCityUfRow, ButtonQuantityDiv, ButtonTrashCart, CartContainer, CartSummary, CartSummaryDiv, CoffeeCardSummary, CoffeeInfosBox, CoffeePaymentInfos, CoffeesSelectedBox, NumberComplementRow, PaymentOptionButton, PaymentOptionsDiv, PaymentSection, PricesInfo, TitlePriceInfo, TitlesSections, TotalPricesInfo } from "./styles";
 import { useTheme } from "styled-components";
+import { Capuccino } from "../../CoffeImport";
 
 export function Cart() {
   const theme = useTheme();
@@ -36,16 +37,95 @@ export function Cart() {
         </AddressSection>
 
         <PaymentSection>
-          <h2>Pagamento</h2>
+          <div>
+            <span><CurrencyDollar size={24} color={theme['purple']} /></span>
+            <div>
+              <h3>Pagamento</h3>
+              <p>O pagamento é feito na entrega. Escolha a forma que deseja pagar</p>
+            </div>
+          </div>
+          <PaymentOptionsDiv>
+            <PaymentOptionButton>
+              <span><CreditCard size={20} color={theme['purple']} /></span> CARTÃO DE CRÉDITO
+            </PaymentOptionButton>
+            <PaymentOptionButton>
+              <span><Bank size={20} color={theme['purple']} /></span> CARTÃO DE DÉBITO
+            </PaymentOptionButton>
+            <PaymentOptionButton>
+              <span><Money size={20} color={theme['purple']} /></span> DINHEIRO
+            </PaymentOptionButton>
+          </PaymentOptionsDiv>
         </PaymentSection>
       </AddressPaymentBox>
 
       <CartSummary>
         <TitlesSections>Cafés selecionados</TitlesSections>
 
-        <section>
-          café
-        </section>
+        <CartSummaryDiv>
+
+          <CoffeesSelectedBox>
+            <CoffeeCardSummary>
+              <img src={Capuccino} alt="coffee" />
+              <CoffeeInfosBox>
+                <p>Expresso Tradicional</p>
+
+                <div>
+                  <ButtonQuantityDiv>
+                    <button>-</button>
+                    <p>{1}</p>
+                    <button>+</button>
+                  </ButtonQuantityDiv>
+                  <ButtonTrashCart>
+                    <Trash size={16} color={theme['purple']} />
+                    REMOVER
+                  </ButtonTrashCart>
+                </div>
+
+              </CoffeeInfosBox>
+              <span>R$ 9,90</span>
+            </CoffeeCardSummary>
+
+            <CoffeeCardSummary>
+              <img src={Capuccino} alt="coffee" />
+              <CoffeeInfosBox>
+                <p>Expresso Tradicional</p>
+
+                <div>
+                  <ButtonQuantityDiv>
+                    <button>-</button>
+                    <p>{1}</p>
+                    <button>+</button>
+                  </ButtonQuantityDiv>
+                  <ButtonTrashCart>
+                    <Trash size={16} color={theme['purple']} />
+                    REMOVER
+                  </ButtonTrashCart>
+                </div>
+
+              </CoffeeInfosBox>
+              <span>R$ 9,90</span>
+            </CoffeeCardSummary>
+          </CoffeesSelectedBox>
+
+          <CoffeePaymentInfos>
+            <div>
+              <div>
+                <TitlePriceInfo>Total de itens</TitlePriceInfo>
+                <PricesInfo>R$ 29,70</PricesInfo>
+              </div>
+              <div>
+                <TitlePriceInfo>Entrega</TitlePriceInfo>
+                <PricesInfo>R$ 3,50</PricesInfo>
+              </div>
+              <div>
+                <TotalPricesInfo>Total</TotalPricesInfo>
+                <TotalPricesInfo>R$ 33,20</TotalPricesInfo>
+              </div>
+            </div>
+            <button>CONFIRMAR PEDIDO</button>
+          </CoffeePaymentInfos>
+
+        </CartSummaryDiv>
       </CartSummary>
 
     </CartContainer>
--- src/pages/Cart/styles.ts
+++ src/pages/Cart/styles.ts
@@ -31,7 +31,7 @@ export const AddressSection = styled.section`
     gap: 0.5rem;
 
     h3 {
-      font-size: 1.08rem;
+      font-size: 1.1rem;
       font-weight: 500;
       color: ${(props) => props.theme["base-subtitle"]};
     }
@@ -54,11 +54,11 @@ export const AddressSection = styled.section`
       padding: 0.75rem;
       border: 1px solid ${(props) => props.theme["base-button"]};
       border-radius: 4px;
-      background-color: ${(props) => props.theme["base-input"]};	
+      background-color: ${(props) => props.theme["base-input"]};
     }
 
     input[name="CEP"] {
-      max-width: 25%;
+      max-width: 33%;
     }
   }
 `;
@@ -75,10 +75,221 @@ export const AreaCityUfRow = styled.div`
   gap: 0.5rem;
 `;
 
-export const PaymentSection = styled.section``;
+export const PaymentSection = styled.section`
+  background-color: ${(props) => props.theme["base-card"]};
+  padding: 1.5rem;
+  border-radius: 8px;
+  margin-top: 1rem;
+
+  div:first-child {
+    display: flex;
+    gap: 0.5rem;
+
+    h3 {
+      font-size: 1.1rem;
+      font-weight: 500;
+      color: ${(props) => props.theme["base-subtitle"]};
+    }
+
+    div {
+      display: flex;
+      flex-direction: column;
+      gap: 0.3rem;
+      margin-bottom: 2rem;
+    }
+  }
+`;
+
+export const PaymentOptionsDiv = styled.div`
+  display: flex;
+  gap: 1rem;
+`;
+
+export const PaymentOptionButton = styled.div`
+  width: 100%;
+  max-width: 200px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  border-radius: 8px;
+  padding: 1rem;
+  color: ${(props) => props.theme["base-text"]};
+  background-color: ${(props) => props.theme["base-button"]};
+
+  font-size: 0.75rem;
+  white-space: nowrap;
+  cursor: pointer;
+
+  span {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-right: 0.5rem;
+  }
+
+  &:hover {
+    background-color: ${(props) => props.theme["base-hover"]};
+  }
+`;
 
 export const CartSummary = styled.div`
   display: flex;
   flex-direction: column;
   gap: 1rem;
+`;
+
+export const CartSummaryDiv = styled.div`
+  background-color: ${(props) => props.theme["base-card"]};
+  padding: 2.5rem;
+  border-radius: 6px;
+
+  border-bottom-left-radius: 2.5rem;
+  border-top-right-radius: 2.5rem;
+`;
+
+export const CoffeesSelectedBox = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const CoffeeCardSummary = styled.div`
+  display: flex;
+  border-bottom: 1px solid ${(props) => props.theme["base-button"]};
+  padding: 2rem 0;
+
+  &:first-child {
+    padding-top: 0.75rem;
+  }
+
+  p {
+    font-size: 1rem;
+    color: ${(props) => props.theme["base-subtitle"]};
+    font-weight: 500;
+    padding-bottom: 8px;
+  }
+
+  img {
+    width: 4rem;
+    margin-right: 1rem;
+  }
+
+  span {
+    width: 100%;
+    display: flex;
+    justify-content: flex-end;
+    font-weight: 600;
+  }
+`;
+
+export const CoffeeInfosBox = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  div {
+    display: flex;
+  }
+`;
+
+export const ButtonQuantityDiv = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${(props) => props.theme["base-button"]};
+  border-radius: 6px;
+  padding: 0 6px;
+  margin-right: 8px;
+
+  button:first-child {
+    font-size: 1.8rem;
+  }
+
+  button {
+    color: ${(props) => props.theme["purple"]};
+    background: none;
+    border: none;
+    font-size: 1.25rem;
+    cursor: pointer;
+    padding: 0 5px;
+    height: 100%;
+
+    &:hover {
+      color: ${(props) => props.theme["purple-dark"]};
+    }
+
+    &:focus {
+      outline: none;
+    }
+  }
+
+  p {
+    color: ${(props) => props.theme["base-title"]};
+    font-weight: 500;
+    padding: 0 5px;
+  }
+`;
+
+export const ButtonTrashCart = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${(props) => props.theme["base-button"]};
+  border-radius: 6px;
+  border: 0;
+  color: ${(props) => props.theme["base-text"]};
+  font-weight: 500;
+  padding: 4px 8px;
+  gap: 4px;
+
+  font-size: 12px;
+
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${(props) => props.theme["base-hover"]};
+  }
+`;
+
+export const CoffeePaymentInfos = styled.div`
+  padding-top: 20px;
+
+  div > div {
+    display: flex;
+    justify-content: space-between;
+
+    padding-bottom: 12px;
+  }
+
+  button {
+    width: 100%;
+    height: 46px;
+    margin-top: 12px;
+    font-size: 14px;
+    font-weight: 500;
+
+    border: none;
+    border-radius: 6px;
+    cursor: pointer;
+    color: ${(props) => props.theme["white"]};
+    background-color: ${(props) => props.theme["yellow"]};
+    transition: 0.5s;
+
+    &:hover {
+      background-color: ${(props) => props.theme["yellow-dark"]};
+    }
+  }
+`;
+
+export const TitlePriceInfo = styled.p`
+  font-size: 14px;
+`;
+
+export const PricesInfo = styled.p`
+  font-size: 16px;
+`;
+
+export const TotalPricesInfo = styled.span`
+  font-size: 18px;
+  font-weight: 600;
 `;
